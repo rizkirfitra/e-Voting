e8a4227a61bf41628a22bd450786a20cd902b2bf
rizkir.fitra <rizkirfitra@student.ub.ac.id>
2020-12-10 19:22:13 +0700

    Change To EazeGraph Chart

--- app/src/main/java/com/ikriz/e_voting/ui/status/StatusFragment.kt
+++ app/src/main/java/com/ikriz/e_voting/ui/status/StatusFragment.kt
@@ -1,23 +1,22 @@
 package com.ikriz.e_voting.ui.status
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.anychart.AnyChart
-import com.anychart.enums.Align
-import com.anychart.enums.LegendLayout
 import com.ikriz.e_voting.R
-import kotlinx.android.synthetic.main.fragment_status.*
-import kotlinx.android.synthetic.main.fragment_status.view.*
+import org.eazegraph.lib.charts.PieChart
 
 
 class StatusFragment : Fragment() {
 
     private lateinit var statusViewModel: StatusViewModel
+    private lateinit var pieChart: PieChart
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,23 +26,16 @@ class StatusFragment : Fragment() {
         statusViewModel = ViewModelProvider(this).get(StatusViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_status, container, false)
         statusViewModel.vote.observe(viewLifecycleOwner, Observer {
-            root.chart.setProgressBar(progress_bar)
-            val pie = AnyChart.pie()
-
-            pie.data(it)
-            pie.title("Quick Count")
-            pie.labels().position("outside")
-            pie.legend().title().enabled(true)
-            pie.legend().title()
-                .text("Retail channels")
-                .padding(0.0, 0.0, 10.0, 0.0)
-            pie.legend()
-                .position("center-bottom")
-                .itemsLayout(LegendLayout.HORIZONTAL)
-                .align(Align.CENTER)
-
-            root.chart.setChart(pie)
+            pieChart = root.findViewById(R.id.chart)
+            for (pieModel in it) pieChart.addPieSlice(pieModel)
+            Log.d("PIE", it.size.toString())
         })
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        pieChart = view?.findViewById(R.id.chart)!!
+        pieChart.startAnimation()
     }
 }
--- app/src/main/java/com/ikriz/e_voting/ui/status/StatusViewModel.kt
+++ app/src/main/java/com/ikriz/e_voting/ui/status/StatusViewModel.kt
@@ -1,40 +1,38 @@
 package com.ikriz.e_voting.ui.status
 
+import android.graphics.Color
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.anychart.chart.common.dataentry.DataEntry
-import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import org.eazegraph.lib.models.PieModel
 
 class StatusViewModel : ViewModel() {
 
     private val db = Firebase.firestore
-    private val data = ArrayList<DataEntry>()
-    private val _vote = MutableLiveData<ArrayList<DataEntry>>()
+    private val data = ArrayList<PieModel>()
+    private val _vote = MutableLiveData<ArrayList<PieModel>>()
 
     init {
-        db.collection("Users").get().addOnSuccessListener {
-            val counter1 = 0
-            val counter2 = 0
-            val counter3 = 0
-            for (doc in it) when (doc["vote"]) {
-                1 -> counter1.plus(1)
-                2 -> counter2.plus(1)
-                3 -> counter2.plus(1)
+        db.collection("Users").get(Source.CACHE).addOnSuccessListener {
+            var counter1 = 0
+            var counter2 = 0
+            var counter3 = 0
+            for (doc in it) when (doc.get("vote")) {
+                "1" -> counter1 += 1
+                "2" -> counter2 += 1
+                "3" -> counter3 += 1
             }
-            data += ValueDataEntry("Paslon 1",counter1)
-            data += ValueDataEntry("Paslon 2",counter2)
-            data += ValueDataEntry("Paslon 3",counter3)
+            data += PieModel("Paslon 1", counter1.toFloat(), Color.parseColor("#FE6DA8"))
+            data += PieModel("Paslon 2", counter2.toFloat(), Color.parseColor("#56B7F1"))
+            data += PieModel("Paslon 3", counter3.toFloat(), Color.parseColor("#CDA67F"))
             _vote.value = data
             Log.d("ENRTY", data.size.toString())
             Log.d("_VOTE", _vote.value!!.size.toString())
         }
-
     }
-
-    val vote: LiveData<ArrayList<DataEntry>> = _vote
-
+    val vote: LiveData<ArrayList<PieModel>> = _vote
 }
